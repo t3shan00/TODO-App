a85061c837ba3a720f9a8faeeec13404c4efffc3
Anil <t3shan00@students.oamk.fi>
2024-03-25 00:56:23 +0200

    part 6

--- server/index.js
+++ server/index.js
@@ -1,61 +1,102 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const { Pool } = require('pg')
+const { query } = require('./helpers/db.js')
+// const { Pool } = require('pg')
+
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-const port = 3001
+const port = process.env.PORT
 
-app.get("/", (req, res) => {
-    const pool = openDb()
+// app.get("/", (req, res) => {
+//     const pool = openDb()
 
-    pool.query('SELECT * FROM task', (error, result) => {
-        if (error) {
-            res.status(500).json({ error: error.message })
-        }
-        res.status(200).json(result.rows)
-    })
+//     pool.query('SELECT * FROM task', (error, result) => {
+//         if (error) {
+//             res.status(500).json({ error: error.message })
+//         }
+//         res.status(200).json(result.rows)
+//     })
+// })
+
+app.get("/", async (req, res) => {
+    console.log(query)
+    try{
+        const result = await query('select * from task')
+        const rows = result.rows ? result.rows : []
+        res.status(200).json(rows)
+    } catch (error) {
+        console.log(error)
+        res.statusMessage = error
+        res.status(500).json({error: error})
+    }
 })
 
-app.post("/new", (req,res) => {
-    const pool = openDb()
+// app.post("/new", (req,res) => {
+//     const pool = openDb()
 
-   pool.query('insert into task (description) values ($1) returning *',
-   [req.body.description],
-    (error,result) => {
-        if (error) {
-            res.status(500).json({error: error.message})
-        } else {
-            res.status(200).json({id : result.rows[0].id})
-        }
-    })
+//    pool.query('insert into task (description) values ($1) returning *',
+//    [req.body.description],
+//     (error,result) => {
+//         if (error) {
+//             res.status(500).json({error: error.message})
+//         } else {
+//             res.status(200).json({id : result.rows[0].id})
+//         }
+//     })
+// })
+
+app.post("/new", async (req, res) => {
+    try {
+        const result = await query('insert into task (description) values ($1) returning *',
+        [req.body.description])
+        res.status(200).json({id:result.rows[0].id})
+    } catch (error) {
+        console.log(error)
+        res.statusMessage = error
+        res.status(500).json({error: error})
+    }
 })
 
-app.delete("/delete/:id",async(req,res) => {
-    const pool = openDb()
-    const id = parseInt(req.params.id)
-    pool.query('delete from task where id = $1',
-    [id],
-    (error,result) => {
-        if (error) {
-            res.status(500).json({error: error.message})
-        } else {
-            res.status(200).json({id: id})
-        }
-    })
+// app.delete("/delete/:id",async(req,res) => {
+//     const pool = openDb()
+//     const id = parseInt(req.params.id)
+//     pool.query('delete from task where id = $1',
+//     [id],
+//     (error,result) => {
+//         if (error) {
+//             res.status(500).json({error: error.message})
+//         } else {
+//             res.status(200).json({id: id})
+//         }
+//     })
+// })
+
+app.delete("/delete/:id", async (req, res) => {
+    const id = Number(req.params.id)
+    try {
+        const result = await query('delete from task where id = $1',
+        [id])
+        res.status(200).json({id: id})
+    } catch (error) {
+        console.log(error)
+        res.statusMessage = error
+        res.status(500).json({error: error})
+    }
 })
 
-const openDb = () => {
-    const pool = new Pool ({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'todo',
-        password: 'admin1234',
-        port: 5432
-    })
-    return pool
-}
+// const openDb = () => {
+//     const pool = new Pool ({
+//         user: process.env.DB_USER,
+//         host: process.env.DB_HOST,
+//         database: process.env.DB_NAME,
+//         password: process.env.DB_PASSWORD,
+//         port: process.env.DB_PORT
+//     })
+//     return pool
+// }
 
 app.listen(port)
