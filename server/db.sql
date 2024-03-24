653f3eb3d9e62615a5b8549ffc7b714823c8f636
Anil <t3shan00@students.oamk.fi>
2024-03-24 21:10:39 +0200

    a

---
+++ server/db.sql
@@ -0,0 +1,13 @@
+drop database if exists todo;
+
+create database todo;
+
+use todo;
+
+create table task(
+    id int primary key auto_increment,
+    description varchar(255) not null
+);
+
+insert into task (description) values ('My test task');
+insert into task (description) values ('My another task');
