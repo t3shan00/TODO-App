14cc9fd2fd700c264e2ed765309640cb9ac85a34
Anil <t3shan00@students.oamk.fi>
2024-03-25 09:27:36 +0200

    env.vault added

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -19,7 +19,8 @@ const openDb = () => {
         host: process.env.DB_HOST,
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT,
+        ssl: process.env.SSL
     })
     return pool
 }
