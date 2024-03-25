cf782eda038d1a3449349603c25532a4d204f034
Anil <t3shan00@students.oamk.fi>
2024-03-25 09:58:43 +0200

    server

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const BACKEND_ROOT_URL = 'http://localhost:3001'
+const BACKEND_ROOT_URL = 'https://todo-backend-qyh8.onrender.com'
 import { Todos } from "./Class/Todos.js"
 
 const todos = new Todos(BACKEND_ROOT_URL)
